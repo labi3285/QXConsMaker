99975d214bbadae000110b38de6c3dcf4ddb9e3e
qinxin <qinxin@shanlinjinrong.com>
2018-03-05 10:29:36 +0800

    tag 0.0.5

--- QXConsMaker/QXConsMaker/QXConsMakerExt.swift
+++ QXConsMaker/QXConsMaker/QXConsMakerExt.swift
@@ -8,62 +8,19 @@
 
 import UIKit
 
-@discardableResult public func MAKE_EQUAL_WIDTH(_ views: UIView ...) -> [NSLayoutConstraint] {
-    var cones = [NSLayoutConstraint]()
-    var first: UIView? = nil
-    for v in views {
-        if let fv = first {
-            cones.append(v.WIDTH.EQUAL(fv).MAKE())
-            first = v
-        } else {
-            first = v
-        }
-    }
-    return cones
-}
-
-@discardableResult public func MAKE_EQUAL_HEIGHT(_ views: UIView ...) -> [NSLayoutConstraint] {
-    var cones = [NSLayoutConstraint]()
-    var first: UIView? = nil
-    for v in views {
-        if let fv = first {
-            cones.append(v.HEIGHT.EQUAL(fv).MAKE())
-            first = v
-        } else {
-            first = v
-        }
-    }
-    return cones
-}
-
-@discardableResult public func MAKE_EQUAL_SIZE(_ views: UIView ...) -> [NSLayoutConstraint] {
-    var cones = [NSLayoutConstraint]()
-    var first: UIView? = nil
-    for v in views {
-        if let fv = first {
-            cones.append(v.WIDTH.EQUAL(fv).MAKE())
-            cones.append(v.HEIGHT.EQUAL(fv).MAKE())
-            first = v
-        } else {
-            first = v
-        }
-    }
-    return cones
-}
-
-extension UIView {
+public extension UIView {
     
-    func IN(_ V: UIView) -> QXConsesMaker        { return QXConsesMaker.updateMaker(view: self, containerView: V, isIn: true, possition: nil) }
+    public func IN(_ V: UIView) -> QXConsesMaker        { return QXConsesMaker.updateMaker(view: self, containerView: V, isIn: true, possition: nil) }
     
-    func TOP(_ V: UIView) -> QXConsesMaker       { return QXConsesMaker.updateMaker(view: self, containerView: V, isIn: false, possition: .top) }
+    public func TOP(_ V: UIView) -> QXConsesMaker       { return QXConsesMaker.updateMaker(view: self, containerView: V, isIn: false, possition: .top) }
     
-    func LEFT(_ V: UIView) -> QXConsesMaker      { return QXConsesMaker.updateMaker(view: self, containerView: V, isIn: false, possition: .left) }
+    public func LEFT(_ V: UIView) -> QXConsesMaker      { return QXConsesMaker.updateMaker(view: self, containerView: V, isIn: false, possition: .left) }
     
-    func BOTTOM(_ V: UIView) -> QXConsesMaker    { return QXConsesMaker.updateMaker(view: self, containerView: V, isIn: false, possition: .bottom) }
+    public func BOTTOM(_ V: UIView) -> QXConsesMaker    { return QXConsesMaker.updateMaker(view: self, containerView: V, isIn: false, possition: .bottom) }
     
-    func RIGHT(_ V: UIView) -> QXConsesMaker     { return QXConsesMaker.updateMaker(view: self, containerView: V, isIn: false, possition: .right) }
+    public func RIGHT(_ V: UIView) -> QXConsesMaker     { return QXConsesMaker.updateMaker(view: self, containerView: V, isIn: false, possition: .right) }
     
-    @discardableResult  func MAKE(W w: CGFloat, H h: CGFloat) -> [NSLayoutConstraint] {
+    @discardableResult public func MAKE(W w: CGFloat, H h: CGFloat) -> [NSLayoutConstraint] {
         let cons1 = self.WIDTH.EQUAL(w).MAKE()
         let cons2 = self.HEIGHT.EQUAL(h).MAKE()
         return [cons1, cons2]
@@ -71,40 +28,40 @@ extension UIView {
     
 }
 
-extension QXConsesMaker {
+public extension QXConsesMaker {
     
-    func SIZE(_ W: CGFloat, _ H: CGFloat) -> QXConsesMaker { return WIDTH(W).HEIGHT(H) }
+    public func SIZE(_ W: CGFloat, _ H: CGFloat) -> QXConsesMaker { return WIDTH(W).HEIGHT(H) }
     
-    func TOP(_ T: CGFloat) -> QXConsesMaker       { return QXConsesMaker.updateMaker(top: T) }
-    func LEFT(_ L: CGFloat) -> QXConsesMaker      { return QXConsesMaker.updateMaker(left: L) }
-    func BOTTOM(_ B: CGFloat) -> QXConsesMaker    { return QXConsesMaker.updateMaker(bottom: B) }
-    func RIGHT(_ R: CGFloat) -> QXConsesMaker     { return QXConsesMaker.updateMaker(right: R) }
-    func WIDTH(_ W: CGFloat) -> QXConsesMaker     { return QXConsesMaker.updateMaker(width: W) }
-    func HEIGHT(_ H: CGFloat) -> QXConsesMaker    { return QXConsesMaker.updateMaker(height: H) }
+    public func TOP(_ T: CGFloat) -> QXConsesMaker       { return QXConsesMaker.updateMaker(top: T) }
+    public func LEFT(_ L: CGFloat) -> QXConsesMaker      { return QXConsesMaker.updateMaker(left: L) }
+    public func BOTTOM(_ B: CGFloat) -> QXConsesMaker    { return QXConsesMaker.updateMaker(bottom: B) }
+    public func RIGHT(_ R: CGFloat) -> QXConsesMaker     { return QXConsesMaker.updateMaker(right: R) }
+    public func WIDTH(_ W: CGFloat) -> QXConsesMaker     { return QXConsesMaker.updateMaker(width: W) }
+    public func HEIGHT(_ H: CGFloat) -> QXConsesMaker    { return QXConsesMaker.updateMaker(height: H) }
     
-    func OFFSET(_ O: CGFloat) -> QXConsesMaker    { return QXConsesMaker.updateMaker(offset: O) }
+    public func OFFSET(_ O: CGFloat) -> QXConsesMaker    { return QXConsesMaker.updateMaker(offset: O) }
     
-    var CENTER: QXConsesMaker                   { return QXConsesMaker.updateMaker(center: true) }
-    var CENTER_X: QXConsesMaker                 { return QXConsesMaker.updateMaker(centerX: true) }
-    var CENTER_Y: QXConsesMaker                 { return QXConsesMaker.updateMaker(centerY: true) }
+    public var CENTER: QXConsesMaker                   { return QXConsesMaker.updateMaker(center: true) }
+    public var CENTER_X: QXConsesMaker                 { return QXConsesMaker.updateMaker(centerX: true) }
+    public var CENTER_Y: QXConsesMaker                 { return QXConsesMaker.updateMaker(centerY: true) }
     
-    var LEFT: QXConsesMaker                     { return LEFT(0) }
-    var RIGHT: QXConsesMaker                    { return RIGHT(0) }
-    var TOP: QXConsesMaker                      { return TOP(0) }
-    var BOTTOM: QXConsesMaker                   { return BOTTOM(0) }
+    public var LEFT: QXConsesMaker                     { return LEFT(0) }
+    public var RIGHT: QXConsesMaker                    { return RIGHT(0) }
+    public var TOP: QXConsesMaker                      { return TOP(0) }
+    public var BOTTOM: QXConsesMaker                   { return BOTTOM(0) }
     
     @discardableResult func MAKE() -> [NSLayoutConstraint]         { return QXConsesMaker.makeUpConstraints() }
     
 }
 
-enum QXConsesPossion {
+public enum QXConsesPossion {
     case top
     case left
     case bottom
     case right
 }
 
-struct QXConsesMaker {
+public struct QXConsesMaker {
     
     fileprivate var isIn: Bool = true
     fileprivate weak var view: UIView?
@@ -127,7 +84,7 @@ struct QXConsesMaker {
     
     fileprivate static var maker = QXConsesMaker()
     
-    static func updateMaker(view v: UIView, containerView cv: UIView, isIn i: Bool, possition p: QXConsesPossion?) -> QXConsesMaker {
+    fileprivate static func updateMaker(view v: UIView, containerView cv: UIView, isIn i: Bool, possition p: QXConsesPossion?) -> QXConsesMaker {
         self.maker.isIn = i
         self.maker.view = v
         self.maker.containerView = cv
@@ -146,57 +103,57 @@ struct QXConsesMaker {
         return self.maker
     }
     
-    static func updateMaker(left l: CGFloat) -> QXConsesMaker {
+    fileprivate static func updateMaker(left l: CGFloat) -> QXConsesMaker {
         self.maker.leftMargin = QXConsesMaker.maker.isIn ? l : -l
         return self.maker
     }
     
-    static func updateMaker(right r: CGFloat) -> QXConsesMaker {
+    fileprivate static func updateMaker(right r: CGFloat) -> QXConsesMaker {
         self.maker.rightMargin = QXConsesMaker.maker.isIn ? -r : r
         return self.maker
     }
     
-    static func updateMaker(top t: CGFloat) -> QXConsesMaker {
+    fileprivate static func updateMaker(top t: CGFloat) -> QXConsesMaker {
         self.maker.topMargin = QXConsesMaker.maker.isIn ? t : -t
         return self.maker
     }
     
-    static func updateMaker(bottom b: CGFloat) -> QXConsesMaker {
+    fileprivate static func updateMaker(bottom b: CGFloat) -> QXConsesMaker {
         self.maker.bottomMargin = QXConsesMaker.maker.isIn ? -b : b
         return self.maker
     }
     
-    static func updateMaker(offset o: CGFloat) -> QXConsesMaker {
+    fileprivate static func updateMaker(offset o: CGFloat) -> QXConsesMaker {
         self.maker.offset = o
         return self.maker
     }
     
-    static func updateMaker(width w: CGFloat) -> QXConsesMaker {
+    fileprivate static func updateMaker(width w: CGFloat) -> QXConsesMaker {
         self.maker.width = w
         return self.maker
     }
     
-    static func updateMaker(height h: CGFloat) -> QXConsesMaker {
+    fileprivate static func updateMaker(height h: CGFloat) -> QXConsesMaker {
         self.maker.height = h
         return self.maker
     }
     
-    static func updateMaker(centerX x: Bool) -> QXConsesMaker {
+    fileprivate static func updateMaker(centerX x: Bool) -> QXConsesMaker {
         self.maker.centerX = x
         return self.maker
     }
     
-    static func updateMaker(centerY y: Bool) -> QXConsesMaker {
+    fileprivate static func updateMaker(centerY y: Bool) -> QXConsesMaker {
         self.maker.centerY = y
         return self.maker
     }
     
-    static func updateMaker(center c: Bool) -> QXConsesMaker {
+    fileprivate static func updateMaker(center c: Bool) -> QXConsesMaker {
         self.maker.center = c
         return self.maker
     }
     
-    static func makeUpConstraints() -> [NSLayoutConstraint] {
+    fileprivate static func makeUpConstraints() -> [NSLayoutConstraint] {
         
         let view = QXConsesMaker.maker.view!
         let containerView = QXConsesMaker.maker.containerView!
